714768b8be8a7c5806dd69df15888ad1769139ee
Hylke van der Schaaf <hylke.vds@gmail.com>
2025-07-09 09:07:48 +0200

    Added initial plugin structure

---
+++ csaf/__init__.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from netbox.plugins import PluginConfig
+from .populate import REPO
+
+from django.core.signals import request_started
+
+
+class NetBoxCsafConfig(PluginConfig):
+    """
+    Plugin config for the CSAF-Plugin initiating the CustomFields and CustomFieldChoiceSets.
+    """
+
+    name = 'csaf'
+    verbose_name = 'NetBox CSAF'
+    description = 'Manage CSAF advisories in NetBox'
+    version = '0.1.0'
+    base_url = 'csaf'
+
+config = NetBoxCsafConfig
+
+
